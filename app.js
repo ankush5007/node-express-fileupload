de89a22033f8e3c1945416701e6cb814412e09d4
ankush5007 <ankush.jetly@yahoo.com>
2020-07-04 15:23:17 +0530

    import lib

--- app.js
+++ app.js
@@ -2,7 +2,35 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+/**
+ * Middleware for logging HTTP Requests
+ */
 var logger = require('morgan');
+/**
+ * Body-parser is a node.js middleware parse the incoming request body before handle and make 
+ * avaialable in req.body object.basically this simplifies the incoming request
+ */
+const bodyParser = require('body-parser');
+
+/**
+ * handle the cross origin resource sharing request
+ */
+const cors = require('cors');
+/**
+ * expres-fileupload is node.js middleware to handle multipart form data extract the files if avaiable
+ * and make available in req.files same this simplfies the incoming request
+ */
+
+const fileUpload = require('express-fileupload');
+
+/**
+ * A js library that provide utltity function to handle Js native data types
+ */
+
+const _ = require('lodash');
+
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,7 +45,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(fileUpload({
+  createParentPath : true,
+}));
+console.log('envirnoment is ' + app.get('env'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
